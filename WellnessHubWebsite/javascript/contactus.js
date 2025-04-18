c20d49f8e4921ae0fd2fb9a82c7d5a78ecfc8a9d
PatrickMuorah <pm00058@my.westga.edu>
2025-04-18 18:50:56 +0100

    I added a functionality to clear the contact form once a user clicks submit

--- WellnessHubWebsite/javascript/contactus.js
+++ WellnessHubWebsite/javascript/contactus.js
@@ -3,4 +3,5 @@ const contactForm = document.getElementById('contactForm');
 contactForm.addEventListener('submit', function(event) {
   event.preventDefault();
   alert('Message sent successfully!');
+  contactForm.reset();
 });

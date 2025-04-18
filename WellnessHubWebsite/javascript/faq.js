39e51ae6a720a43f041bfc5d395c28a84e9fb9ca
PatrickMuorah <pm00058@my.westga.edu>
2025-04-18 21:23:46 +0100

    I created a modal window for the quick tips section too. I deleted irrelevant code in the css and the help1 image as I thought a hero image won't be necessary

--- WellnessHubWebsite/javascript/faq.js
+++ WellnessHubWebsite/javascript/faq.js
@@ -1,22 +1,37 @@
-// Select modal elements
 const faqModal = document.getElementById('faqModal');
-const faqTrigger = document.querySelector('.faq-link'); // The clickable div/link
+const faqTrigger = document.querySelector('.faq-link');
 const closeButton = document.querySelector('.close-button');
 
-// Open the modal when the clickable div is clicked
 faqTrigger.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent default link behavior
-  faqModal.style.display = 'block'; // Show the modal
+  event.preventDefault();
+  faqModal.style.display = 'block';
 });
 
-// Close the modal when the close button is clicked
 closeButton.addEventListener('click', () => {
-  faqModal.style.display = 'none'; // Hide the modal
+  faqModal.style.display = 'none';
 });
 
-// Close the modal by clicking outside the modal content
 window.addEventListener('click', (event) => {
   if (event.target === faqModal) {
     faqModal.style.display = 'none';
+  }
+});
+
+const tipsModal = document.getElementById('tipsModal');
+const tipsTrigger = document.getElementById('tipsTrigger');
+const tipsCloseButton = document.querySelector('.tips-close');
+
+tipsTrigger.addEventListener('click', (event) => {
+  event.preventDefault();
+  tipsModal.style.display = 'block';
+});
+
+tipsCloseButton.addEventListener('click', () => {
+  tipsModal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === tipsModal) {
+    tipsModal.style.display = 'none';
   }
 });
